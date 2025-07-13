0924b9e60db74ae9ff5a799f279331f72c1f9f20
SniprAF <sniprafai@gmail.com>
2025-07-13 14:21:39 +0330

    Create snipr_token_fetcher.py

---
+++ snipr_token_fetcher.py
@@ -0,0 +1,11 @@
+import requests
+
+url = "https://api.dexscreener.com/latest/dex/pairs/solana"
+response = requests.get(url)
+data = response.json()
+
+# گرفتن توکن‌های تازه لیست شده
+for i, token in enumerate(data["pairs"][:10]):
+    print(f"{i+1}. {token['baseToken']['name']} ({token['baseToken']['symbol']}) - Price: {token['priceUsd']}")
+
+added initial token fetcher
